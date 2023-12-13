64e4fcbcfb8feedfd50f6bae49acdae52afc9e74
Inv <artem@tarabakin.ru>
2023-12-13 14:58:06 +0300

    corrected

--- src/Main.java
+++ src/Main.java
@@ -4,37 +4,37 @@ public class Main {
     public static void main(String[] args) {
 
         List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
-        List<String> msg = new ArrayList<>(List.of("word","java","word","java"));
-        List<String> msg2 = new ArrayList<>(List.of("один", "два","два", "три", "три", "три"));
+        List<String> msg = new ArrayList<>(List.of("word", "java", "word", "java"));
+        List<String> msg2 = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
         task1(nums);
         task2(nums);
         task3(msg);
         task4(msg2);
-
-
     }
 
-    public static void task1(List<Integer> nums){
+    public static void task1(List<Integer> nums) {
         for (int i = 0; i < nums.size(); i++) {
-            if(nums.get(i) % 2 != 0){
+            if (nums.get(i) % 2 != 0) {
                 System.out.println(nums.get(i));
             }
         }
         System.out.println("_______________________");
     }
 
-    public static void task2(List<Integer> nums){
+    public static void task2(List<Integer> nums) {
         Set<Integer> set = new HashSet<>(nums);
         nums.clear();
         nums.addAll(set);
+
         for (int i = 0; i < nums.size(); i++) {
-            if(nums.get(i) % 2 != 0){
+            if (nums.get(i) % 2 != 0) {
                 System.out.println(nums.get(i));
             }
         }
         System.out.println("_______________________");
     }
-    public static void task3(List<String> nums){
+
+    public static void task3(List<String> nums) {
         Set<String> set = new HashSet<>(nums);
         nums.clear();
         nums.addAll(set);
@@ -44,12 +44,10 @@ public class Main {
         System.out.println("_______________________");
     }
 
-    public static void task4(List<String> nums){
-        Set<String> unique = new HashSet<String>(nums); // в конструктор кладем наш список
+    public static void task4(List<String> nums) {
+        Set<String> unique = new HashSet<String>(nums);
         for (String key : unique) {
             System.out.println(key + ":" + Collections.frequency(nums, key));
         }
     }
-
-
 }
